55e56b0e5194f28cd8506e25a3d3f16807f0b7e7
tapos-chandro <taposchandro231@gmail.com>
2024-12-07 19:58:15 +0600

    Aid for Injured in the Quota Movement has been done

--- js/index.js
+++ js/index.js
@@ -85,4 +85,45 @@ document.getElementById('donate-btn-two').addEventListener('click', function(e){
 })
 
 // Donate for Flood at Noakhali, Bangladesh end 
+// Donate for Flood at Noakhali, Bangladesh start 
+
+document.getElementById('donate-btn-three').addEventListener('click', function(e){
+    
+    const donateInputAmount = getInputAmount('donate-amount-three')
+    const donateAmount = getInnerTextValue('donate-total-amount-three')
+    const grandTotal = getInnerTextValue('grand-total-amount')  
+
+    e.stopPropagation()
+    e.preventDefault()
+    const form = document.querySelectorAll("form")[2];
+    if (!form.checkValidity()) {
+        e.preventDefault(); 
+        form.reportValidity(); 
+        return
+    }
+    const validateInputAmount = getInputValidations(donateInputAmount)
+
+    if(validateInputAmount){
+
+        if(grandTotal >= validateInputAmount){
+            updateGrandTotalAmount('grand-total-amount', grandTotal, validateInputAmount)
+            
+        }else{
+            alert('Your amount very low, places add amount and try it')
+            return
+        }
+     
+        // document.getElementById().innerText = validateInputAmount + donateAmount;
+        getTotalDonateAmount('donate-total-amount-three', validateInputAmount , donateAmount )
+
+        const donateTitle = getDonateTitle('donate-title-three')
+
+
+        appendDonateHistory('history-container', (validateInputAmount) , donateTitle)
+         my_modal_5.showModal()
+    }
+
+})
+
+// Donate for Flood at Noakhali, Bangladesh end 
 
